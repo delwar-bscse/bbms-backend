09a5c85a9583aa6be1621c1cbb9b0dca422cb2e1
Md. Delwar Hossain <delwarbscse@gmail.com>
2024-07-16 17:48:20 +0600

    Initialize BBMS App Backend

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+//rest object
+const app = express();
+
+//routes
+//1 test route
+app.get("/", (req,res)=>{
+  res.status(200).json({
+    message: "Welcome to Blood Bank App"
+  })
+})
+
+//port
+const PORT = 8080;
+
+//listen
+app.listen(PORT, ()=>{
+  console.log("Node server is running");
+})
