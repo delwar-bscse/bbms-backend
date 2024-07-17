582217720947fb7ca06bad55aebf22c08b42e84b
Md. Delwar Hossain <delwarbscse@gmail.com>
2024-07-17 16:04:42 +0600

    user, admin, organization, hospital model created

---
+++ models/userModel.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: [true, "Role is required"],
+    enum: ['user','admin','organization','hospital']
+  },
+  name: {
+    type: String,
+    required: function(){
+      if(this.role === 'user' || this.role === 'admin'){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  organization: {
+    type: String,
+    required: function(){
+      if(this.role === 'organization'){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  hospital: {
+    type: String,
+    required: function(){
+      if(this.role === 'hospital'){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"]
+  },
+  website: {
+    type: String
+  },
+  address: {
+    type: String,
+    required: [true, "Address is required"]
+  },
+  Phone: {
+    type: String,
+    required: [true, "Phone is required"]
+  },
+},{timestamps:true});
+
+const userModel = mongoose.model('users',userSchema);
+
+export default userModel;
